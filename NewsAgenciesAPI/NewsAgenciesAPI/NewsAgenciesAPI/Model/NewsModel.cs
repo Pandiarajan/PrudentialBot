3e31088ba17e3161e38f151f1ce0f1e57a918786
Neha Pawar <nehaapawar21@gmail.com>
2023-06-25 19:19:54 +0530

    API and Web service

---
+++ NewsAgenciesAPI/NewsAgenciesAPI/NewsAgenciesAPI/Model/NewsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NewsAgenciesAPI.Model
+{
+    public class NewsModel
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+    }
+}
